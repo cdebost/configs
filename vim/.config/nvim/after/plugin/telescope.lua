5af148544d3acd150d24480f8039d5506f964ec1
Corentin Debost <codebost@gmail.com>
2024-11-21 23:43:23 -0800

    Live grep

--- vim/.config/nvim/after/plugin/telescope.lua
+++ vim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>P', builtin.find_files, {})
 vim.keymap.set('n', '<leader>p', builtin.git_files, {})
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>;', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
